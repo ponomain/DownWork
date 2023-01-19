916fa1e64c7bffece6e0abb73dbcb416a884eca1
nikolai <ponomarev.main@gmail.com>
2023-01-19 16:57:31 +0400

    m1l1: add changes into gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
-    application
+    kotlin("jvm")
 }
 
 group = "ru.otus.otuskotlin.downwork"
 version = "0.0.1"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
 
 subprojects {
     group = rootProject.group
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
 rootProject.name = "DownWork"
+
+
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
 include("down-work-service")
