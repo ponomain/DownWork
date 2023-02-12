1856ca0955dc75a4ed9f84f7a11a23cc712e9fab
nikolai <ponomarev.main@gmail.com>
2023-02-12 17:56:37 +0400

    m2h1: change README.md and add changes into global gradle files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,15 @@ rootProject.name = "DownWork"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val kotestVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        id("io.kotest.multiplatform") version kotestVersion apply false
     }
 }
-include("down-work-service")
+include("down-work-service",
+    "down-work-app",
+    "down-work-repository",
+    "down-work-web")
